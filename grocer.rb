71cba761be4c4aede93f72a12353e21a49d90eee
Jinming Zhang <jinming.zhang.92@gmail.com>
2019-08-26 03:01:34 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,13 +1,51 @@
 def consolidate_cart(cart)
-  # code here
+  c_cart = {}
+  cart.map do |item|
+    if c_cart[item.keys[0]]
+      c_cart[item.keys[0]][:count] += 1
+    else
+      c_cart[item.keys[0]] = {
+        count: 1,
+        price: item.values[0][:price],
+        clearance: item.values[0][:clearance]
+      }
+    end
+  end
+  c_cart
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  coupons.each do |coupon|
+    if cart.keys.include? coupon[:item]
+      if cart[coupon[:item]][:count] >= coupon[:num]
+        new_name = "#{coupon[:item]} W/COUPON"
+        if cart[new_name]
+          cart[new_name][:count] += coupon[:num]
+        else
+          cart[new_name] = {
+            count: coupon[:num],
+            price: coupon[:cost]/coupon[:num],
+            clearance: cart[coupon[:item]][:clearance]
+          }
+        end
+        cart[coupon[:item]][:count] -= coupon[:num]
+      end
+    end
+  end
+  cart
 end
 
 def apply_clearance(cart)
-  # code here
+  clear_cart={}
+  cart.each do |clear|
+    if clear[:clearance]==true
+      clear[:price]=0.8*clear[:price].round(2)
+      clear_cart.push(clear[:price])
+    else
+      clear_cart.push(clear[:price])
+    end
+  end
+    clear_cart
 end
 
 def checkout(cart, coupons)
