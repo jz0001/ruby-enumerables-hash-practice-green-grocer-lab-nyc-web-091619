c070c728166722bac17931cca489930c19e5294e
Jinming Zhang <jinming.zhang.92@gmail.com>
2019-08-26 03:19:40 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -52,7 +52,7 @@ def checkout(cart, coupons)
   c_cart_coup_clear = apply_clearance(c_cart_coup)
   
   sum=0
-  c_cart_coup_clear.keys.each.do |thingz|
+  c_cart_coup_clear.keys.each do |thingz|
     sum=((c_cart_coup_clear[thingz][:price]*c_cart_coup_clear[thingz][:count]).round(2))+sum
   end
   # sum=(sum*0.9).round(2) if sum>100
