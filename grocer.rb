401df5bcae44bed78aae32f514cf85c715361e8a
Jinming Zhang <jinming.zhang.92@gmail.com>
2019-08-26 03:34:37 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -70,13 +70,13 @@ def checkout(cart, coupons)
   #   subtotal += cart3[element][:price]*cart3[element][:count]
   # end
   
-  consol_cart = consolidate_cart(cart)
-  cart_with_coupons_applied = apply_coupons(consol_cart, coupons)
-  cart_with_discounts_applied = apply_clearance(cart_with_coupons_applied)
+  # consol_cart = consolidate_cart(cart)
+  # cart_with_coupons_applied = apply_coupons(consol_cart, coupons)
+  # cart_with_discounts_applied = apply_clearance(cart_with_coupons_applied)
 
-  total = 0.0
-  cart_with_discounts_applied.keys.each do |item|
-    total += cart_with_discounts_applied[item][:price]*cart_with_discounts_applied[item][:count]
-  end
+  # total = 0.0
+  # cart_with_discounts_applied.keys.each do |item|
+  #   total += cart_with_discounts_applied[item][:price]*cart_with_discounts_applied[item][:count]
+  # end
 end
     
