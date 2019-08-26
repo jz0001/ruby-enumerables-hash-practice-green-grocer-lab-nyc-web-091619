4249fee2eeb605ac0ced626503316630a8ed7130
Jinming Zhang <jinming.zhang.92@gmail.com>
2019-08-26 03:27:32 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -59,14 +59,14 @@ def checkout(cart, coupons)
   #   sum+=(c_cart_coup_clear[thingz][:price]*c_cart_coup_clear[thingz][:count]).round(2)
   # end
   # subtotal=(subtotal*0.9).round(2) if subtotal>100
-  consol_cart = consolidate_cart(cart)
-  cart_with_coupons_applied = apply_coupons(consol_cart, coupons)
-  cart_with_discounts_applied = apply_clearance(cart_with_coupons_applied)
+  cart1 = consolidate_cart(cart)
+  cart2 = apply_coupons(cart1, coupons)
+  cart3 = apply_clearance(cart2)
 
-  total = 0.0
-  cart_with_discounts_applied.keys.each do |item|
-    total += cart_with_discounts_applied[item][:price]*cart_with_discounts_applied[item][:count]
+  subtotal = 0.00
+  cart3.keys.each do |element|
+    subtotal += (cart3[element][:price]*cart3[element][:count]).round(2)
   end
-  total > 100.00 ? (total * 0.90).round : total
+   > 100.00 ? (total * 0.90).round : total
 end
     
